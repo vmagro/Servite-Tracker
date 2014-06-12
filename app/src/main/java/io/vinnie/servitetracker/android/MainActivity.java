f854ef935d43f2d5b6b138f2de5222dee3c2c070
Vinnie Magro <vmagro@socaldevs.com>
2014-06-12 08:45:23 -0700

    Drawer open & close from app icon

--- app/src/main/java/io/vinnie/servitetracker/android/MainActivity.java
+++ app/src/main/java/io/vinnie/servitetracker/android/MainActivity.java
@@ -3,9 +3,12 @@ package io.vinnie.servitetracker.android;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -16,6 +19,7 @@ public class MainActivity extends Activity implements DrawerAdapter.DrawerItemLi
     private DrawerAdapter drawerAdapter = null;
     private ArrayList<Fragment> drawerFragments = new ArrayList<Fragment>();
     private DrawerLayout drawerLayout = null;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,20 @@ public class MainActivity extends Activity implements DrawerAdapter.DrawerItemLi
 
     private void setupDrawer() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        );
+
+        // Set the drawer toggle as the DrawerListener
+        drawerLayout.setDrawerListener(mDrawerToggle);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
         ListView drawer = (ListView) findViewById(R.id.drawer);
 
         String[] classes = getResources().getStringArray(R.array.drawer_fragment_classes);
@@ -92,5 +110,30 @@ public class MainActivity extends Activity implements DrawerAdapter.DrawerItemLi
         if (active instanceof TitleProvider) {
             setTitle(((TitleProvider) active).getTitle(this));
         }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
     }
 }
